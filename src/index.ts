d75406b1f0f61021a520dafa0add5052223cbe13
dislersd <dislersd@gmail.com>
2020-08-23 14:14:23 -0700

    set up graphql with apollo server

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,25 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import microConfig from "./mikro-orm.config";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
+// main is wrapper function to provide top level async/await
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
   const app = express();
 
-  app.get("/", (_, res) => {
-    res.send("Hello");
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
   });
+
+  apolloServer.applyMiddleware({ app });
 
   app.listen(4000, () => {
     console.log("\n *** Server running on localhost:4000 *** \n");
