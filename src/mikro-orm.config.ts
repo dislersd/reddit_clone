cd956cb4ed4d19128b59f987b0ccdf7e0cbdcf8c
dislersd <dislersd@gmail.com>
2020-08-26 14:15:01 -0700

    created User entity

--- src/mikro-orm.config.ts
+++ src/mikro-orm.config.ts
@@ -2,6 +2,7 @@ import { Post } from "./entities/Post";
 import { __prod__ } from "./constants";
 import { MikroORM } from "@mikro-orm/core";
 import path from "path";
+import { User } from "./entities/User";
 
 export default {
   migrations: {
@@ -10,7 +11,7 @@ export default {
     // pattern matching for a .ts or .js file [tj]s = ts or js
     pattern: /^[\w-]+\d+\.[tj]s$/,
   },
-  entities: [Post],
+  entities: [Post, User],
   dbName: "redditclone",
   type: "postgresql",
   debug: !__prod__,
