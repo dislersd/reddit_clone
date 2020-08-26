86173a1e65566e51e918064a63de62f9a8dd1c5c
dislersd <dislersd@gmail.com>
2020-08-25 21:02:44 -0700

    created mutations to create and update posts

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,4 +1,4 @@
-import { Resolver, Query, Ctx, Arg, Int } from "type-graphql";
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from "type-graphql";
 import { Post } from "../entities/Post";
 import { MyContext } from "../types";
 
@@ -15,5 +15,32 @@ export class PostResolver {
     @Ctx() { em }: MyContext
   ): Promise<Post | null> {
     return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg("title") title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title });
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post, {nullable: true})
+  async updatePost(
+    @Arg("id") id: number,
+    @Arg("title", () => String, {nullable: true}) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, {id})
+    if (!post) {
+      return null
+    }
+    if (typeof title !== "undefined") {
+      post.title = title
+      await em.persistAndFlush(post)
+    }
+    return post;
   }
 }
