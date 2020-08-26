1d5649c231f00335461877b0b467f726dd14bbea
dislersd <dislersd@gmail.com>
2020-08-25 21:07:47 -0700

    created mutation to delete a post - full CRUD on posts complete

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -27,20 +27,29 @@ export class PostResolver {
     return post;
   }
 
-  @Mutation(() => Post, {nullable: true})
+  @Mutation(() => Post, { nullable: true })
   async updatePost(
     @Arg("id") id: number,
-    @Arg("title", () => String, {nullable: true}) title: string,
+    @Arg("title", () => String, { nullable: true }) title: string,
     @Ctx() { em }: MyContext
   ): Promise<Post | null> {
-    const post = await em.findOne(Post, {id})
+    const post = await em.findOne(Post, { id });
     if (!post) {
-      return null
+      return null;
     }
     if (typeof title !== "undefined") {
-      post.title = title
-      await em.persistAndFlush(post)
+      post.title = title;
+      await em.persistAndFlush(post);
     }
     return post;
+  }
+
+  @Mutation(() => Boolean)
+  async deletePost(
+    @Arg("id") id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<boolean> {
+    await em.nativeDelete(Post, { id });
+    return true;
   }
 }
